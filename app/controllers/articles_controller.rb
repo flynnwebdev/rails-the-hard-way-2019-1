1389391c8848bfa84626eafbb493a01e4582be71
MattFlynn <flynnwebdev@gmail.com>
2019-04-04 14:29:18 +1000

    Finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,12 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def new
+        @article = Article.new
+    end
+    def index
+        @articles = Article.all
+        # render plain: @articles.inspect
     end
     def create
         # Get the permitted parameters and
@@ -7,23 +14,42 @@ class ArticlesController < ApplicationController
         # Store the instance in @article
         @article = Article.new(article_params)
         # Commit the new article to the database
-        @article.save
+        if @article.save # validation happens here
+            redirect_to @article
+        else
+            render 'new'  # render the new article form
+        end
         # Redirect the client browser to the page to
         # view the newly created article
-        redirect_to @article
     end
     def show
         # Get the id from the URL and use it
         # to look up the Article with that id.
         # If found, store a new instance of that Article
         # in @article
-        @article = Article.find(params[:id])
         # render plain: @article.inspect
+    end
+    def edit
+    end
+    def update
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+        # render plain: params.inspect
+    end
+    def destroy
+        @article.destroy
+        redirect_to articles_path
     end
 
     private
 
     def article_params
         params.require(:article).permit(:title, :content)
+    end
+    def set_article
+        @article = Article.find(params[:id])
     end
 end
